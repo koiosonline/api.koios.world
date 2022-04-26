48c424abb1ec19a00a56f270399eaabb777e1657
Leslie Lauw <lauw2498@live.nl>
2022-04-26 20:11:43 +0200

    updates

--- handlers/merkleClaimHandler.js
+++ handlers/merkleClaimHandler.js
@@ -4,23 +4,27 @@ import Web3 from "web3";
 import whitelistAddresses from "../data/addresses.json" assert { type: "json" };
 
 const merkleClaimHandler = async ({ claimAddress, tokenId }) => {
-  const whitelistAddressesLeaves = whitelistAddresses.claims.map((x) =>
-    Web3.utils.soliditySha3(x.tokenID, x.owner)
-  );
-  const merkleTree = new MerkleTree(whitelistAddressesLeaves, keccak256, {
-    sortPairs: true,
-  });
+  try {
+    const whitelistAddressesLeaves = whitelistAddresses.claims.map((x) =>
+      Web3.utils.soliditySha3(x.tokenID, x.owner)
+    );
+    const merkleTree = new MerkleTree(whitelistAddressesLeaves, keccak256, {
+      sortPairs: true,
+    });
 
-  const whitelistRootHash = merkleTree.getHexRoot();
-  console.log("Whitelist Root Hash: " + whitelistRootHash);
+    const whitelistRootHash = merkleTree.getHexRoot();
+    console.log("Whitelist Root Hash: " + whitelistRootHash);
 
-  console.log(claimAddress + " : " + tokenId);
+    console.log(claimAddress + " : " + tokenId);
 
-  const hashedAddress = Web3.utils.soliditySha3(tokenId, claimAddress);
-  const proof = merkleTree.getHexProof(hashedAddress);
-  console.log(proof);
+    const hashedAddress = Web3.utils.soliditySha3(tokenId, claimAddress);
+    const proof = merkleTree.getHexProof(hashedAddress);
+    console.log(proof);
 
-  return { proof: proof };
+    return { proof: proof };
+  } catch (e) {
+    return { proos: e };
+  }
 };
 
 export default merkleClaimHandler;
