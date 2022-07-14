50756398cbfdddf42f456fdd81a5a3b38912bc1d
Leslie Lauw <lauw2498@live.nl>
2022-07-14 13:46:30 +0200

    added test env

--- src/api/db/connectMongo.ts
+++ src/api/db/connectMongo.ts
@@ -1,9 +1,16 @@
+import e from "express";
 import mongoose from "mongoose";
 
 export const connectMongo = async () => {
   if (mongoose.connection.readyState === 1) {
     mongoose.connections[0];
     return;
+  } else {
+    if (process.env.NODE_ENV === "test") {
+      mongoose.connect(process.env.MONGO_URI_TEST);
+      console.log("ENV == " + process.env.NODE_ENV);
+    } else {
+      mongoose.connect(process.env.MONGO_URI_DEV);
+    }
   }
-  mongoose.connect(process.env.MONGO_URI);
 };
