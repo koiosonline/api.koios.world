a2330d07a3b31690803ba2e3912685eb25ab49fb
Leslie Lauw <lauw2498@live.nl>
2022-07-18 16:53:08 +0200

    temp

--- src/api/db/connectMongo.ts
+++ src/api/db/connectMongo.ts
@@ -6,6 +6,7 @@ export const connectMongo = async () => {
     return;
   } else {
     if (process.env.NODE_ENV === "test") {
+      console.log(process.env.MONGO_URI_TEST);
       mongoose.connect(process.env.MONGO_URI_TEST);
     } else {
       mongoose.connect(process.env.MONGO_URI_DEV);
